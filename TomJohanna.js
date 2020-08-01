0ce7dec370c673d4c7955a9b70bf9d12bc4d2377
Tom Coysman <65373256+sterrenstof-code@users.noreply.github.com>
2020-08-01 16:30:16 +0200

    Add files via upload

--- TomJohanna.js
+++ TomJohanna.js
@@ -45,7 +45,7 @@ jQuery(function($) {
 
 const one = document.querySelector('.one');
 const uitlegCardsOne = document.querySelector('.uitlegCardsOne');
-const uitlegBgOne = document.querySelector('.uitleg-bg-one');
+
 
 let scrollAmountOne = 0;
 let oldscrollAmountOne = 0;
@@ -59,14 +59,14 @@ window.addEventListener('scroll', function() {
       }
       one.style.transform = `translateY(${scrollAmountOne * 1}px)`;
       uitlegCardsOne.style.transform = `translateY(${scrollAmountOne * 1}px)`;
-      uitlegBgOne.style.transform = `translateY(${scrollAmountOne * 1}px)`;
+   
     };  
   oldscrollAmountOne = scrollTopOne;
 });
 
 const two = document.querySelector('.two');
 const uitlegCardsTwo = document.querySelector('.uitlegCardsTwo');
-const uitlegBgTwo = document.querySelector('.uitleg-bg-two');
+
 
 let scrollAmountTwo = 0;
 let oldscrollAmountTwo = 0;
@@ -80,7 +80,7 @@ window.addEventListener('scroll', function() {
       }
       two.style.transform = `translateY(${scrollAmountTwo * 1}px)`;
       uitlegCardsTwo.style.transform = `translateY(${scrollAmountOne * 1}px)`;
-      uitlegBgTwo.style.transform = `translateY(${scrollAmountOne * 1}px)`;
+
     };  
   oldscrollAmountTwo = scrollTop;
 });
@@ -88,7 +88,7 @@ window.addEventListener('scroll', function() {
 
 const three = document.querySelector('.three');
 const uitlegCardsThree = document.querySelector('.uitlegCardsThree');
-const uitlegBgThree = document.querySelector('.uitleg-bg-three');
+
 
 let scrollAmountThree = 0;
 let oldscrollAmountThree = 0;
@@ -102,7 +102,7 @@ window.addEventListener('scroll', function() {
       }
       three.style.transform = `translateY(${scrollAmountThree * 1}px)`;
       uitlegCardsThree.style.transform = `translateY(${scrollAmountOne * 1}px)`;
-      uitlegBgThree.style.transform = `translateY(${scrollAmountOne * 1}px)`;
+
     };  
   oldscrollAmountThree = scrollTop;
 });
